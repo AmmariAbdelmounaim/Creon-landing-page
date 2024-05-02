c80a7abcc60afae58b345996692b44574c2152e9
AmmariAbdelmounaim <abdelmounaim.ammari@gmail.com>
2024-05-02 08:27:57 +0100

    Update font sizes in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,32 @@ const config: Config = {
           line: "#13171D",
         },
       },
+      fontSize: {
+        sm: [
+          "1.125rem",
+          {
+            lineHeight: "1.2375rem",
+          },
+        ],
+        md: [
+          "1.375rem",
+          {
+            lineHeight: "1.7875rem",
+          },
+        ],
+        lg: [
+          "2.375rem",
+          {
+            lineHeight: "2.85rem",
+          },
+        ],
+        xl: [
+          "4.25rem",
+          {
+            lineHeight: "4.675rem",
+          },
+        ],
+      },
       fontFamily: {
         satoshi: ["var(--font-satoshi)"],
         monument_extended: ["var(--font-monument-extended)"],
